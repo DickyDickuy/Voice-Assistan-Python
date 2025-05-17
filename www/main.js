6531d04d48114d9610eb3c5e3dd90245fbdc8012
DickyDickuy <deathbreaker3035@gmail.com>
2025-05-18 06:03:02 +0700

    day3 complete

--- www/main.js
+++ www/main.js
@@ -11,4 +11,28 @@ $(document).ready(function () {
         },
     });
 
+    // siri configuration
+      var siriWave = new SiriWave({
+    container: document.getElementById("siri-container"),
+    width: 640,
+    height: 200,
+    style: "ios9",
+    amplitude: 1,
+    speed: 0.03,
+    autostart: true,
+        });
+
+    // Siri message animation
+        $('.siri-message').textillate({
+        loop: true,
+        sync: true,
+        in:{
+            effect: "fadeInUp",
+            sync: true,
+        },
+        out: {
+            effect: "fadeOutUp",
+            sync: true,
+        },
+    });
 });
